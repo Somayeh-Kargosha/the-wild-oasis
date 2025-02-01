bed22d4f9349f8938b1ef946a81b6471f120a2aa
Somayeh-Kargosha <95878619+Somayeh-Kargosha@users.noreply.github.com>
2025-02-01 18:02:40 +0330

    Update supabase.js

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://ytjkyrxxwvibvbpogzou.supabase.co";
+export const supabaseUrl = "https://ytjkyrxxwvibvbpogzou.supabase.co";
 
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0amt5cnh4d3ZpYnZicG9nem91Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4MTM0NTcsImV4cCI6MjA0NjM4OTQ1N30.ZMwfjWm1Q_DCoYVnc45bfudLfDliZGqDYvtExCghaZ0";
